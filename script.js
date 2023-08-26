36332f21d13b69da7b32effe1078afd8803d5b2b
johnblackbinary <104975467+yahyaHaghshenas@users.noreply.github.com>
2023-08-26 19:51:15 +0800

    latest movies and style enhanced
    showing the latest movies based on load, showing titles, details on style

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 const form = document.querySelector('#searchForm');
+const imagesContainer = document.querySelector('.images-container');
+
+
 form.addEventListener('submit', async function(e){
     e.preventDefault();
     const searchTerm = form.elements.query.value;
     const config = {params : {q: searchTerm}};
     const res = await axios.get(`http://api.tvmaze.com/search/shows/`, config);
-    // console.log(res.data[0].show.image.medium);
+    
     // const img = document.createElement('IMG');
     // img.src = res.data[0].show.image.medium;
     // document.body.append(img);
@@ -13,24 +16,43 @@ form.addEventListener('submit', async function(e){
 })
 
 const makeImages = (shows) => {
-    const imagesContainer = document.querySelector('.images-container');
+    imagesContainer.innerHTML = ''
     for (result of shows){
-        
         if (result.show.image){
-            console.log("there is")
+            // console.log(result.show.name)
+            const imgContainer = document.createElement('div');
+            imgContainer.classList.add('image-container');
+
+
             const img = document.createElement('IMG');
             img.src = result.show.image.medium;
             img.classList.add('grid-image');
-            imagesContainer.appendChild(img);
+            imgContainer.appendChild(img);
             //document.body.append(img);
+
+            const title = document.createElement('p');
+            title.textContent = result.show.name;
+            title.classList.add('image-title');
+            imgContainer.appendChild(title);
+
+            imagesContainer.appendChild(imgContainer);
         }
         
     }
 }
 
+function getCurrentDate() {
+    const currentDate = new Date();
+    const year = currentDate.getFullYear();
+    const month = String(currentDate.getMonth()+1).padStart(2, '0');
+    const day = String(currentDate.getDate()).padStart(2, '0');
+    return `${year}-${month}-${day}`;
+}
+
 const getLatestTvShows = async () => {
     try {
-        const resLate = await axios.get(`http://api.tvmaze.com/schedule?country=US&date=2023-08-01`);
+        const today = getCurrentDate();
+        const resLate = await axios.get(`http://api.tvmaze.com/schedule?country=US&date=`,today);
         makeImages(resLate.data);
     } catch(e) {
         console.log("EROOR", e);
